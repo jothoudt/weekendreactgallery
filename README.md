83ca35e0b3c4b860b8f2a875a0bca6616a56e45f
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-03-26 23:09:32 -0500

    display photos

--- README.md
+++ README.md
@@ -6,4 +6,8 @@
 
 Your project description goes here. What problem did you solve? How did you solve it?
 
+[x] create a Axios Get call to retrieve information
+[x] create new component GalleryList and move information there via props
+[x] create a new component GalleryItem and move information there via props
+
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './GalleryItem.css'
 
 function GalleryItem(props){
     return(
         <>
-        <><p key={props.galleryItem.id}>{props.galleryItem.path} {props.galleryItem.description}{props.galleryItem.likes}</p></>
+        <img src={props.galleryItem.path}></img>
+        <><p key={props.galleryItem.id}>{props.galleryItem.description}{props.galleryItem.likes}</p></>
         </>
     )
 }
